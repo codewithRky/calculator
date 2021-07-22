fba038919af717e261444b1b8d9094b06a011d0b
NitinKhandpa <83783806+NitinKhandpa@users.noreply.github.com>
2021-07-22 21:30:48 +0530

    Update index.php
    Updated 11

--- index.php
+++ index.php
@@ -1,18 +1,64 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator</title>
+    <title>Document</title>
+
+    <style>
+        .meracalci{
+            align-items: center;
+            text-align: center;
+            border:1px solid black;
+        }
+
+        .apnatext {
+            padding: 2px -8px;
+    margin: 8px 3px;
+    font-size: 13px;
+    text-align: center;
+    position: relative;
+    left: 585px;
+    align-items: center;
+    width: 35px;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>Calculator</h1>
-<<<<<<< HEAD
+    <div class="meracalci">
+    <table>
+        <form>
+            <tr><input type="text" name="Mytext"></tr>
+        <tr>
+            <td><input type="button" class="apnatext" name="Mytext" value=7></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=8></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=9></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="/"></td>
+        </tr>
+        <tr>
+            <td><input type="button" class="apnatext" name="Mytext" value=4></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=5></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=6></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="x"></td>
+        </tr>
+        <tr>
+            <td><input type="button" class="apnatext" name="Mytext" value=1></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=2></td>
+            <td><input type="button" class="apnatext" name="Mytext" value=3></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="-"></td>
+        </tr>
+        <tr>
+            <td><input type="button" class="apnatext" name="Mytext" value=0></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="."></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="+"></td>
+            <td><input type="button" class="apnatext" name="Mytext" value="="></td>
+        </tr>
+    </form>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aspernatur, aperiam necessitatibus porro iusto consequuntur laudantium eos enim voluptas illum.</p>
-=======
-    
->>>>>>> 50736a99e847f955d22651c908beed19f1f33e89
+
+    </table>
+    </div>
 </body>
-</html> -->
+</html>
