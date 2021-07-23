ed1533b30b8e52d81db74e8eae3020f2692fec57
rupesh9661 <75015846+rupesh9661@users.noreply.github.com>
2021-07-23 09:07:56 +0530

    updated

--- script.js
+++ script.js
@@ -1,117 +1,33 @@
    
          
-      function show0(){
-        const two= document.getElementById("zero").value;
+      function show(num){
+        
     
         var output= document.getElementById("outputfield"); 
-        output.value+=" "+ two;
+        output.value+= num;
 
          }
-      function show1(){
-        const one= document.getElementById("one").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
+         function equal(){
+           var exp= document.getElementById("outputfield").value;
+           
+          
+            var result=  eval(exp);
+          
+      
+            document.getElementById("outputfield").value=result;
+           
+      
          }
-      function show2(){
-        const one= document.getElementById("two").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
+         function backspace(){
+           var exp= document.getElementById("outputfield").value;
+           
+           length= exp.length;
+           
+          modified= exp.slice(0, (length-1));
+           document.getElementById("outputfield").value=modified;
+           
          }
-      function show3(){
-        const one= document.getElementById("three").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
+         function clearall(){
+          document.getElementById("outputfield").value=" ";
          }
-      function show4(){
-        const one= document.getElementById("four").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function show5(){
-        const one= document.getElementById("five").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function show6(){
-        const one= document.getElementById("six").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function show7(){
-        const one= document.getElementById("seven").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function show8(){
-        const one= document.getElementById("eight").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function show9(){
-        const one= document.getElementById("nine").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function showdiv(){
-        const one= document.getElementById("div").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function showcross(){
-        const one= document.getElementById("cross").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " "+ one;
-
-         }
-      function showplus(){
-        const one= document.getElementById("plus").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function showminus(){
-        const one= document.getElementById("minus").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-        // console.log()
-
-         }
-      function showdecimal(){
-        const one= document.getElementById("decimal").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-
-         }
-      function showequalto(){
-        const one= document.getElementById("equalto").value;
-    
-        var output= document.getElementById("outputfield"); 
-        output.value+= " " + one;
-        console.log(output.value)
-        // console.log(5*3);
-
-         }
+ 
